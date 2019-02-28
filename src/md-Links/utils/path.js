991bc560ffde6b2494424f7c1dabe0572ec27b2f
GloryRojas <grojasm94@gmail.com>
2019-02-27 20:47:08 -0500

    Test para funcion rutaesarchivo

--- src/md-Links/utils/path.js
+++ src/md-Links/utils/path.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 export const rutaEsAbsoluta = (ruta) => {
    return path.isAbsolute(ruta);
@@ -8,3 +9,7 @@ export const transformarAAbsoluta = (ruta) => {
     return path.resolve(ruta);
  };
  
+export const rutaEsArchivo = (ruta) => {
+    let rutilla = fs.statSync(ruta)
+    return rutilla.isFile();
+ };
--- test/path.spec.js
+++ test/path.spec.js
@@ -1,11 +1,11 @@
-import { rutaEsAbsoluta, transformarAAbsoluta } from '../src/md-Links/utils/path.js';
+import { rutaEsAbsoluta, transformarAAbsoluta, rutaEsArchivo } from '../src/md-Links/utils/path.js';
 
 describe('rutaEsAbsoluta', () => {
     it('debería ser una función', () => {
         expect(typeof rutaEsAbsoluta).toBe('function');
     });
     it('Debería retornar true si la ruta es absoluta', () => {
-        expect(rutaEsAbsoluta('C:\\foo\\bar.js')).toBe(true)
+        expect(rutaEsAbsoluta('/foo/bar')).toBe(true)
     });
     it('Debería retornar false si la ruta no es absoluta', () => {
         expect(rutaEsAbsoluta('../foo/bar')).toBe(false)
@@ -16,7 +16,15 @@ describe('rutaEsAbsoluta', () => {
         expect(typeof transformarAAbsoluta).toBe('function');
     });
     it('Debería transformar una ruta relativa en absoluta', () => {
-        expect(transformarAAbsoluta('../foo/bar')).toBe('C:\\Users\\Laboratoria\\Documents\\Projects\\foo\\bar')
+        expect(transformarAAbsoluta('C:\\Users\\Laboratoria\\Documents\\Projects\\foo\\bar')).toBe('C:\\Users\\Laboratoria\\Documents\\Projects\\foo\\bar')
+    });
+  })
+  describe('rutaEsArchivo', () => {
+    it('debería ser una función', () => {
+        expect(typeof rutaEsArchivo).toBe('function');
+    });
+    it('debería devolver true si ruta es archivo', () => {
+        expect(rutaEsArchivo('C:\Users\Laboratoria\Documents\Projects\LIM008-fe-md-links\package.json')).toBe(true);
     });
   })
 
