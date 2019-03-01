ccc91cdb4a1029ca888e9acb0ebc631e036ff08a
GloryRojas <grojasm94@gmail.com>
2019-03-01 10:58:55 -0500

    Funcion recorrer carpeta y devolver array de archivos completada

--- src/md-Links/utils/path.js
+++ src/md-Links/utils/path.js
@@ -1,26 +1,42 @@
 const path = require('path');
 const fs = require('fs');
-
+/*
 const rutaEsAbsoluta = (ruta) => path.isAbsolute(ruta);
 
 const transformarAAbsoluta = (ruta) => path.resolve(ruta);
- 
-const rutaEsArchivo = (ruta) => {
-    let arrayRuta = [];
-    let esArchivo = fs.statSync(ruta).isFile();
-    if (esArchivo === true ){
-        arrayRuta.push(ruta);
-    }
-    return arrayRuta;
- };
+ */
 
 const recorrerCarpeta = (ruta) => {
   let listaArchivos = fs.readdirSync(ruta);
   let arrayArchivos = [];
     for(let i=0; i< listaArchivos.length; i++){
-     arrayArchivos.push(ruta + '\\' + listaArchivos[i]);
+      let rutaArchivos = ruta + '\\' + listaArchivos[i];
+      if(fs.statSync(rutaArchivos).isFile()){
+        arrayArchivos.push(rutaArchivos);
+      }else{
+        arrayArchivos.push(recorrerCarpeta(rutaArchivos));
+      }
     }
   return arrayArchivos;
 }
-console.log(recorrerCarpeta('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links'));
+
+function flattenDeep(arr1) {
+  return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
+}
+
+const rutaEsArchivo = (ruta) => {
+    let arrayRuta = [];
+    let esArchivo = fs.statSync(ruta).isFile();
+    if (esArchivo === true ){
+      arrayRuta.push(ruta);
+    } else{
+      arrayRuta.push(recorrerCarpeta(ruta));
+    }
+    return arrayRuta;
+ };
+
+ console.log(flattenDeep(rutaEsArchivo('C:\\Users\\Glory\\Documents\\Laboratoria 18-19\\LIM008-fe-md-links\\src\\cli.js')));
+ 
+
+//console.log(flattenDeep(recorrerCarpeta('C:\\Users\\Glory\\Documents\\Laboratoria 18-19\\LIM008-fe-md-links\\src')));
 
