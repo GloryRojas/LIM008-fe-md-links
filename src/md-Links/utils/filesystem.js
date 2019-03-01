f3eead6e1c2933007e1c12b907c17188b81e1d71
GloryRojas <grojasm94@gmail.com>
2019-03-01 17:38:08 -0500

    Eliminando funcion flat y modificando funcion recorrer carpeta

--- src/md-Links/utils/filesystem.js
+++ src/md-Links/utils/filesystem.js
@@ -1,31 +1,30 @@
 const fs = require('fs');
 
-export const recorrerCarpeta = (ruta) => {
+const recorrerCarpeta = (ruta) => {
     let listaArchivos = fs.readdirSync(ruta);
     let arrayArchivos = [];
-      for(let i=0; i< listaArchivos.length; i++){
+        for(let i=0; i< listaArchivos.length; i++){
         let rutaArchivos = ruta + '\\' + listaArchivos[i];
         if(fs.statSync(rutaArchivos).isFile()){
-          arrayArchivos.push(rutaArchivos);
+            arrayArchivos.push(rutaArchivos);
         }else{
-          arrayArchivos.push(recorrerCarpeta(rutaArchivos));
+            arrayArchivos = arrayArchivos.concat(recorrerCarpeta(rutaArchivos));
         }
-      }
+        }
     return arrayArchivos;
-  }
+};
+
+ // console.log(recorrerCarpeta('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\src'));
   
-  function flattenDeep(arr1) {
-    return arr1.reduce((acc, val) => Array.isArray(val) ? acc.concat(flattenDeep(val)) : acc.concat(val), []);
-  }
-  
- export const rutaEsArchivo = (ruta) => {
-      let arrayRuta = [];
-      let esArchivo = fs.statSync(ruta).isFile();
-      if (esArchivo === true ){
+const rutaEsArchivo = (ruta) => {
+    let arrayRuta = [];
+    let esArchivo = fs.statSync(ruta).isFile();
+    if (esArchivo === true ){
         arrayRuta.push(ruta);
-      } else{
-        arrayRuta.push(recorrerCarpeta(ruta));
-      }
-      return arrayRuta;
-   };
+    } else{
+        arrayRuta = arrayRuta.concat(recorrerCarpeta(ruta));
+    }
+    return arrayRuta;
+};
   
+console.log(rutaEsArchivo('C:\\Users\\Laboratoria\\Documents\\Projects\\LIM008-fe-md-links\\src'));
