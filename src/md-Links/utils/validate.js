42423670f5994f40d4cc95c7f68f48782bf16a1c
GloryRojas <grojasm94@gmail.com>
2019-03-06 21:06:38 -0500

    Funcion estado de link completado

--- src/md-Links/utils/validate.js
+++ src/md-Links/utils/validate.js
@@ -1,3 +1,17 @@
-export const validarLinks = (arrayLinks) => {
-    
+const fetch = require('node-fetch') ;  
+
+export const estadoLink = (link) => {
+    fetch(link).then((response) => console.log(response.status));
 }
+        //.catch((err) => err.message)
+
+console.log(estadoLink('https://www.npmjs.com/package/node-fetch#iface-body'));
+
+export const validarLinks = (arrayLinks) => {
+    const arrLinks = arrayLinks;
+
+    for(let i=0; i<arrLinks.length; i++){
+        let link = arrLinks[i];
+    
+    }
+ }
