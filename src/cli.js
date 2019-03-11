b1a75cb6333cd09455a6df4de6180ce43e21f0ca
GloryRojas <grojasm94@gmail.com>
2019-03-11 18:31:55 -0500

    CLI para funcion MD-LINKS terminado

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,23 @@
 
 import { mdLinks } from "./md-Links/md-links.js"
 
-const opciones = {
-    validate: true,
-    stats: false
-} 
+//mdLinks('./src', opciones).then(resp => console.log(resp));
 
-mdLinks('./src', opciones).then(resp => console.log(resp));
+const [,, ...args] = process.argv;
+/* console.log(`Hola ${args}`);
+console.log(args[0]);
+console.log(args[1]);
+console.log(args[2]); */
+if (args[1] === '--validate' && args[2] === '--stats'){
+    // console.log('1', opciones);
+    mdLinks(args[0], {validate: true, stats: true}).then(resp => console.log(resp))
+} else if(args[1] === '--validate'){
+    // console.log('2', opciones);  
+    mdLinks(args[0], {validate: true, stats: false}).then(resp => console.log(resp))
+} else if (args[1] === '--stats'){
+    // console.log('3', opciones);
+    mdLinks(args[0], {validate: false, stats: true}).then(resp => console.log(resp))
+} else {
+    // console.log('4', opciones);
+    mdLinks(args[0], {validate: false, stats: false}).then(resp => console.log(resp))
+}
