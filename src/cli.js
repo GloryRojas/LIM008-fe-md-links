d59fc1b243df83b2ff092035af49d6737bc0b833
GloryRojas <grojasm94@gmail.com>
2019-03-12 00:30:40 -0500

    CLI para función md-links

--- src/cli.js
+++ src/cli.js
@@ -1,24 +1,18 @@
 #!/usr/bin/env node
 
-import { mdLinks } from "./md-Links/md-links.js"
-import { estadisticas } from '../src/stats.js';
-//mdLinks('./src', opciones).then(resp => console.log(resp));
+import mdLinks from './md-Links/md-links.js';
+import { estadisticas } from './stats.js';
+import { templateDeResultado } from './md-Links/utils/moduls.js';
 
 const [,, ...args] = process.argv;
-/* console.log(`Hola ${args}`);
-console.log(args[0]);
-console.log(args[1]);
-console.log(args[2]); */
 if (args[1] === '--validate' && args[2] === '--stats'){
-    // console.log('1', opciones);
-    mdLinks(args[0], {validate: true, stats: true}).then(resp => console.log(resp))
-} else if(args[1] === '--validate'){
-    // console.log('2', opciones);  
-    mdLinks(args[0], {validate: true, stats: false}).then(resp => console.log(resp))
+    mdLinks(args[0], {validate: true}).then(resp => console.log(estadisticas(resp, 'validate')))
+} else if (args[1] === '--stats' && args[2] === '--validate'){
+    mdLinks(args[0], {validate: true}).then(resp => console.log(estadisticas(resp, 'validate')))
+} else if(args[1] === '--validate'){  
+    mdLinks(args[0], {validate: true}).then(resp => console.log(templateDeResultado(resp)))
 } else if (args[1] === '--stats'){
-    // console.log('3', opciones);
-    mdLinks(args[0], {validate: false, stats: true}).then(resp => console.log(resp))
+    mdLinks(args[0]).then(resp => console.log(estadisticas(resp)))
 } else {
-    // console.log('4', opciones);
-    mdLinks(args[0], {validate: false, stats: false}).then(resp => console.log(resp))
+    mdLinks(args[0]).then(resp => console.log(templateDeResultado(resp)))
 }
--- src/md-Links/md-links.js
+++ src/md-Links/md-links.js
@@ -2,17 +2,17 @@ import { validarLinks } from './utils/validate.js';
 import { devolverRutaAbsoluta, recorrerCarpeta } from './utils/moduls.js';
 import { extraerLinks } from './utils/link.js';
 
-export const mdLinks = (path, options) => new Promise ((resolve, reject) =>{
+export default (path, options) => new Promise ((resolve, reject) =>{
     const rutaAbs = devolverRutaAbsoluta(path);
     const objLinks = extraerLinks(recorrerCarpeta(rutaAbs));
-    if (options.validate){
+    if (options === undefined){
+        resolve(objLinks)
+    } else if (options.validate){
         validarLinks(objLinks)
         .then(resp => resolve(resp))
-    } else {
-        resolve(objLinks)
     }
 });
 
-/* mdLinks('./src', {validate: true}).then(resp => console.log(resp));
-mdLinks('./src', {validate: false}).then(resp => console.log(resp));
- */
+//mdLinks('./src', {validate: true}).then(resp => console.log(resp));
+//mdLinks('./src').then(resp => console.log(resp));
+ 
