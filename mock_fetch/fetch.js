0f7331a196d160eebadb5308a53cbb2abfda1013
GloryRojas <grojasm94@gmail.com>
2019-03-20 11:46:28 -0500

    Cambios en mock fecth

--- mock_fetch/fetch.js
+++ mock_fetch/fetch.js
@@ -1,14 +1,14 @@
 const fetch = () => new Promise((resolve) => {
-    resolve([ 
-        { href: 'http://as01.epimg.net/epik/imagenes/2018/04/28/portada/1524913221_572475_1524913364_noticia_normal.jpg',
-          text: 'Perrito',
-          file: './ejemplo.md',
-          stat: 'OK',
-          code: 200,
-        }
-      ]);
+  resolve([ 
+    { href: 'http://as01.epimg.net/epik/imagenes/2018/04/28/portada/1524913221_572475_1524913364_noticia_normal.jpg',
+      text: 'Perrito',
+      file: './ejemplo.md',
+      stat: 'OK',
+      code: 200,
+    }
+  ]);
 });
 
 export default jest.fn(() => {
-    return fetch;
-})
+  return fetch;
+});
