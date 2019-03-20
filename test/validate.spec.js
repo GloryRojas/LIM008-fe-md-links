58185cd3fa25be5065957facf0cfb224f905fc60
GloryRojas <grojasm94@gmail.com>
2019-03-19 20:57:58 -0500

    Haciendo Mock para fetch

--- test/validate.spec.js
+++ test/validate.spec.js
@@ -1,15 +1,11 @@
 import { validarLinks } from '../src/md-Links/validate.js';
+import mockFetch from '../mock_fetch/fetch.js';
+global.fetch = mockFetch();
 
 const output3 = [ 
   { href: 'http://as01.epimg.net/epik/imagenes/2018/04/28/portada/1524913221_572475_1524913364_noticia_normal.jpg',
     text: 'Perrito',
-    file: './ejemplo.md' },
-  { href: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBA3ZYqSO6GqIcBnCU0mrW8gCQhSC0OE1UsOqTytQoiqVFSf8z',
-    text: 'Perrito 2',
-    file: './ejemplo.md' },
-  { href: 'https://gloryrojas.github.io/lim-2018-11-bc-core-am-cipher/src/&quot;',
-    text: 'https://gloryrojas.github.io/lim-2018-11-bc-core-am-cipher/src/&quot;',
-    file: './archivo1.md' } 
+    file: './ejemplo.md' }
 ];
 
 const output4 = [ 
@@ -18,18 +14,6 @@ const output4 = [
     file: './ejemplo.md',
     stat: 'OK',
     code: 200,
-  },
-  { href: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBA3ZYqSO6GqIcBnCU0mrW8gCQhSC0OE1UsOqTytQoiqVFSf8z',
-    text: 'Perrito 2',
-    file: './ejemplo.md',
-    stat: 'OK',
-    code: 200,
-  },
-  { href: 'https://gloryrojas.github.io/lim-2018-11-bc-core-am-cipher/src/&quot;',
-    text: 'https://gloryrojas.github.io/lim-2018-11-bc-core-am-cipher/src/&quot;',
-    file: './archivo1.md', 
-    stat: 'FAIL',
-    code: 404
   }
 ];
 
